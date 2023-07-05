83149481bb1e832bea3bf7c7251b9e8c6bcee751
brodriguezcalvo <138702237+brodriguezcalvo@users.noreply.github.com>
2023-07-05 10:10:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,3 @@ streamlit.title('my Parents New Healthy Diner')
 
 streamlit.body('Breakfast Menu')
 
-streamlit.body('Omega 3 & Blueberry Oatmeal')
-
-
-streamlit.body('Kale, Spinach & Rocket Smoothie')
